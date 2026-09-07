43d9a934c71c9bf7c3afcade62c282b7c6d81814
habibbatista <habibpkxd@gmail.com>
2026-09-07 01:38:24 -0400

    Refactor AppDelegate by removing scene configuration
    Removed unused scene configuration method.

--- iOS/RIVEN/AppDelegate.swift
+++ iOS/RIVEN/AppDelegate.swift
@@ -6,23 +6,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
 
         FirebaseApp.configure()
 
         return true
-    }
-
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-
-        return UISceneConfiguration(
-            name: "Default Configuration",
-            sessionRole: connectingSceneSession.role
-        )
     }
 }
